f49d5a9abcb25d44eba68b17b4dedc2650167550
Ian Cornelius <icornelius@luc.edu>
2025-09-18 13:23:10 -0500

    subject categories: update CSV and Py script
    - CSV: begin assigning subject-terms to categories (proof of concept)
    - Py: read `subject-categories.csv` as input

--- scripts/update-subjects.py
+++ scripts/update-subjects.py
@@ -1,9 +1,7 @@
 # This Python script reads subject terms from the WIP crosswalk
-# (`subjects.csv`), reports added and deleted subject terms, and creates a CSV
-# file `subject-categories.csv`, in which terms will be assigned to categories.
-#
-# The script will need to be updated to read `subject-categories.csv`, once we
-# begin filling the 'categories' field in that file.
+# (`subjects.csv`), reports added and deleted subject terms, and merges the
+# changed subject terms into another WIP file, in which we assign subject terms
+# to categories (`subject-categories.csv`).
 
 import os
 import csv
@@ -11,8 +9,7 @@ import csv
 # Root variables
 
 data_dir = '../artefacts/'
-csv_sources = ['subjects.csv']
-categories_csv = 'subject-categories.csv'
+csv_sources = ['subjects.csv', 'subject-categories.csv']
 
 def verify_path(csv_sources, data_dir):
     print('Verifying presence of data files...')
@@ -26,7 +23,8 @@ def verify_path(csv_sources, data_dir):
 
 def load_csv_to_dict(csv_sources, data_dir):
     subject_crosswalk = []
-    destination_list = [subject_crosswalk]
+    subject_categories = []
+    destination_list = [subject_crosswalk, subject_categories]
     n = 0
     while n < len(destination_list):
         file_path = data_dir + csv_sources[n]
@@ -36,7 +34,7 @@ def load_csv_to_dict(csv_sources, data_dir):
             for row in reader:
                 destination.append(row)
         n += 1
-    return subject_crosswalk
+    return subject_crosswalk, subject_categories
 
 def process_subject_terms(subject_crosswalk):
     revised_subjects = set()
@@ -63,32 +61,47 @@ def process_subject_terms(subject_crosswalk):
     print(f'\nFound {str(len(subject_crosswalk))} subject terms in DIMEV 1.0')
     print(f'Of these, {str(len(deleted_subjects))} are marked for deletion')
     print(f'{str(len(added_subjects))} new subject terms will be added, for a total of {str(len(revised_subjects))} subject terms after revision')
-    print(f'\nThe deleted subject terms are: {deleted_subjects}')
-    print(f'\nThe new subject terms are: {added_subjects}')
+    print(f'\nThe deleted subject terms:\n{"; ".join(deleted_subjects)}')
+    print(f'\nThe new subject terms:\n{"; ".join(added_subjects)}')
     return revised_subjects
 
-def create_subject_categories(revised_subjects):
-    subject_categories = []
+def create_subject_categories(revised_subjects, subject_categories):
+    revised_subject_categories = []
     revised_subjects = list(revised_subjects)
     revised_subjects.sort()
-    for item in revised_subjects:
-        subject_categories.append((item, ''))
-    return subject_categories
+    for subject_term in revised_subjects:
+        revised_subject_categories.append({'subject': subject_term, 'category': ''})
+    for base_item in revised_subject_categories:
+        for content_item in subject_categories:
+            if content_item['subject'] == base_item['subject']:
+                base_item['category'] = content_item['category']
+    category_terms = set()
+    unassigned_subject_terms = 0
+    for item in revised_subject_categories:
+        if item['category'] == '':
+            unassigned_subject_terms += 1
+        else:
+            category_terms.add(item['category'])
+    category_terms = list(category_terms)
+    category_terms.sort()
+    print(f'\nFound {str(len(category_terms))} subject categories: {", ".join(category_terms)}')
+    print(f'Found {str(unassigned_subject_terms)} subject terms unassigned to a category')
+    return revised_subject_categories
 
 def write_subject_categories(revised_categories):
-    destination = data_dir + categories_csv
+    destination = data_dir + csv_sources[1]
     with open(destination, 'w') as file:
         file.write('subject,category\n')
         for item in revised_categories:
-            if ',' in item[0]:
-                file.write('"' + item[0] + '",' + item[1] + '\n')
+            if ',' in item['subject']:
+                file.write('"' + item['subject'] + '",' + item['category'] + '\n')
             else:
-                file.write(item[0] + ',' + item[1] + '\n')
-    print(f'\nWrote {str(len(revised_categories))} lines of data to {categories_csv}')
+                file.write(item['subject'] + ',' + item['category'] + '\n')
+    print(f'\nWrote {str(len(revised_categories))} lines of data to {destination}')
 
 verify_path(csv_sources, data_dir)
-subject_crosswalk = load_csv_to_dict(csv_sources, data_dir)
+subject_crosswalk, subject_categories = load_csv_to_dict(csv_sources, data_dir)
 revised_subjects = process_subject_terms(subject_crosswalk)
-revised_categories = create_subject_categories(revised_subjects)
+revised_categories = create_subject_categories(revised_subjects, subject_categories)
 write_subject_categories(revised_categories)
 print('Done')
